87eefef5f243acdd9c6fe71ee75f2df2fdad477d
Bohdan Ses <bohdan.ses@gmail.com>
2022-08-19 18:12:16 +0200

    fixed_bugs with refs

--- src/index.js
+++ src/index.js
@@ -5,29 +5,29 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-refs = {
-    searchList: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info'),
-}
 
-refs.searchList.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
+const searchList = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+    
+
+searchList.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput() {
-    if (refs.searchList === "") {
-        refs.countryList.innerHTML = "";
-        refs.countryInfo.innerHTML = "";
+    if (searchList === "") {
+        countryList.innerHTML = "";
+        countryInfo.innerHTML = "";
         return;
     }
 
-    fetchCountries(refs.searchList.value.trim())
+    fetchCountries(searchList.value.trim())
         .then(result => {
             createCountryList(result);
             console.log(result);
         })
         .catch(error => {
             Notify.failure("Oops, there is no country with that name");
-            refs.countryList.innerHTML = "";
+            countryList.innerHTML = "";
             console.log(error);
         })
 }
@@ -39,12 +39,12 @@ function createCountryList(countries) {
                 <p class = "country-name">${country.name.official}</p>
             </li>
             `).join("");
-    refs.countryList.innerHTML = markup;
-    refs.countryInfo.innerHTML = "";
+    countryList.innerHTML = markup;
+    countryInfo.innerHTML = "";
 
     if (countries.length > 10) {
         Notify.info("Too many matches found. Please enter a more specific name.");
-        refs.countryList.innerHTML = "";
+        countryList.innerHTML = "";
     }
     if (countries.length === 1) {
         performCountryInfo(countries[0]);
@@ -58,5 +58,5 @@ function performCountryInfo(countryObj) {
     <span class = "country-info-data">Population:</span><span>${countryObj.population}</span><br>
     <span class = "country-info-data">Languages:</span><span>${languageMarkup}</span>
     `;
-    refs.countryInfo.innerHTML = infoMarkup;
+    countryInfo.innerHTML = infoMarkup;
 }
